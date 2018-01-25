798b3d514e3f4bb3f6f0f13f09600d80e9bcd695
Keith L Mannock <keith@dcs.bbk.ac.uk>
2018-01-25 16:29:23 +0000

    Worksheet Four

---
+++ worksheets/four/test/BasicsKtTest.kt
@@ -0,0 +1,45 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class BasicsKtTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun inRange() {
+    }
+
+    @Test
+    fun oddRange() {
+    }
+
+    @Test
+    fun minWhile() {
+    }
+
+    @Test
+    fun minFor() {
+    }
+
+    @Test
+    fun minRecursive() {
+    }
+
+    @Test
+    fun base36() {
+    }
+
+    @Test
+    fun splitInHalf() {
+    }
+
+    @Test
+    fun isPalindrome() {
+    }
+
+    @Test
+    fun wordCounter() {
+    }
+}
