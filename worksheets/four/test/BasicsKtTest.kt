6b432fb356dc467d06a089053131fb299e19e971
Keith L Mannock <keith@dcs.bbk.ac.uk>
2018-01-26 22:25:30 +0000

    Worksheet Four

--- worksheets/four/test/BasicsKtTest.kt
+++ worksheets/four/test/BasicsKtTest.kt
@@ -1,6 +1,8 @@
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import java.math.BigInteger
 import java.util.*
 
 class BasicsKtTest {
@@ -44,10 +46,10 @@ class BasicsKtTest {
     @Test
     fun oddRangeTest() {
         for (n in 1..1000) {
-            val r = oddRange(1, n)
-            assertTrue("The first element in your range is not 1", r.start == 1)
+            val r = oddRange(n)
+            assertEquals("The first element in your range is not 1", r.start, 1)
             for (v in r) {
-                assertTrue("You have a non-odd value $v in your range", v % 2 == 1)
+                assertEquals("You have a non-odd value $v in your range", v % 2, 1)
             }
         }
     }
@@ -57,32 +59,77 @@ class BasicsKtTest {
      */
     @Test
     fun minWhileTest() {
-        assertTrue(minWhile(arrayOf(55, 6, 24, 32, 1, 16, 12)) == 1)
-        assertTrue(minWhile(arrayOf(2, 3, 4, 5)) == 2)
-        assertTrue(minWhile(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)) == 12)
+        assertEquals(1, minWhile(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minWhile(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minWhile(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
     }
 
+    /**
+     * The minFor function should return the minimum integer in an array using a for loop
+     */
     @Test
-    fun minFor() {
+    fun minForTest() {
+        assertEquals(1, minFor(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minFor(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minFor(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
     }
 
+    /**
+     * The minRecursive function should return the minimum integer in an array using recursion
+     */
     @Test
-    fun minRecursive() {
+    fun minRecursiveTest() {
+        assertEquals(1, minRecursive(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minRecursive(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minRecursive(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
     }
 
+    /**
+     * The base36 function should return the base 36 equivalent of the given BigInt
+     */
     @Test
-    fun base36() {
+    fun base36Test() {
+        assertEquals("21i3v9", base36(BigInteger("123456789")))
+        assertEquals("4jc8lik", base36(BigInteger("9876543212")))
+        assertEquals("xrls1yk9rf9", base36(BigInteger("123456789123456789")))
     }
 
+    /**
+     * The splitInHalf function should split a string in half
+     */
     @Test
-    fun splitInHalf() {
+    fun splitInHalfTest() {
+        fun check(s: String, ls: String, rs: String) = {
+            val (l, r) = splitInHalf(s)
+            assertEquals("$l is not the same as $ls", ls, l)
+            assertEquals("$r is not the same as $rs", rs, r)
+        }
+        check("racecar", "rac", "ecar")
+        check("sloppy", "slo", "ppy")
     }
 
+    /**
+     * isPalindrome should return true if a string is a palindrome
+     */
     @Test
-    fun isPalindrome() {
+    fun isPalindromeTest() {
+        val pp = arrayOf(
+                "racecar",
+                "A car, a man, a maraca",
+                "A Toyota's a Toyota",
+                "In words, alas, drown I"
+        )
+
+        for (p in pp) {
+            assertTrue("$p is a palindrome", isPalindrome(p))
+        }
     }
 
+    /**
+     * For you to complete...
+     */
     @Test
     fun wordCounter() {
+        TODO()
     }
 }
