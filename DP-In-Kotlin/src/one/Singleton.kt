72af135327bb4f8d27aff578ef4629229c0683da
Keith L Mannock <keith@dcs.bbk.ac.uk>
2018-02-20 15:27:36 +0000

    reorganisation and corrections for DP in Kotlin

---
+++ DP-In-Kotlin/src/one/Singleton.kt
@@ -0,0 +1,15 @@
+package one
+
+object PrinterDriver {
+    init {
+        println("Initializing with object: $this")
+    }
+
+    fun print() = println("Printing with object: $this")
+}
+
+fun main(args: Array<String>) {
+    println("Start")
+    PrinterDriver.print()
+    PrinterDriver.print()
+}
